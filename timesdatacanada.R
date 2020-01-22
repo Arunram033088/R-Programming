b58d582c7d77a95174f1b16c96c067e9175b7518
Arun Ramanathan <arunramanathan3@gmail.com>
2020-01-22 17:41:36 +0530

    Added comments

--- timesdatacanada.R
+++ timesdatacanada.R
@@ -11,3 +11,4 @@ timesdata2016 <- timesdata2016 %>%
   filter(num_students > 25000)
 timesdata2016 <- timesdata2016 %>%
   filter(world_rank < 50 & world_rank != "201-250" & world_rank != "251-300" & world_rank != "301-350" & world_rank != "351-400" & world_rank != "401-500")
+#Filtering out top universities based in Canada with number of students greater than 25000 and world rank less than 200
