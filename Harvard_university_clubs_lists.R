7a2be4fe3828389a620b798ef316147cebc27599
Arun Ramanathan <arunramanathan3@gmail.com>
2020-01-22 14:00:51 +0530

    Update Harvard_university_clubs_lists.R
    Created 2 lists names chess and rugby clubs. Finally added the chess and rugby clubs to uni_list.

--- Harvard_university_clubs_lists.R
+++ Harvard_university_clubs_lists.R
@@ -1,27 +1,30 @@
-# Create a sample list which will have the names of clubs inside the harvard university
-uni_clubs <- list("rugy_club", "ballroom_dancing", "chess_club")
-print(uni_clubs)
-# Adding chess_club to the list of clubs with vectors and matrices
-club_title <- c("Chess Club")
-club_description <- c("Meets two nights a week for members to play chess. Snacks are provided.")
-club_dues <- c(50, 20, 15)
-meeting_days <- c("Monday", "Wednesday")
-meeting_times <- c("6:00 pm", "8:00 pm")
-club_meetings <- rbind(meeting_days,meeting_times)
-chess_club <- list(club_title, club_description, club_dues, club_meetings)
-# Assigning header for the list
-chess_club_names <- c("club_title", "club_description", "club_dues", "club_meetings")
-names(chess_club) <- chess_club_names
-print(chess_club)
-#Indexing a list to get the second value in the club_dues vector
-chess_club[[c(3,2)]]
-#To index a particular element in a list and replacing it
-chess_club[[c(3,3)]] <- 5
-chess_club[[c(3,3)]]
-#Adding a new vector named first_years in to chess_club list and assigining its name and indexing the list to the numbers of student participated in the spring semester
-first_years <- c(12, 15)
-names(first_years) <- c("fall", "spring")
-chess_club[[5]] <- first_years
-chess_club[["first_years"]] <- first_years
-chess_club[[c(5,2)]]
-print(chess_club)
+rclub_title <- c("Rugbby_Club") #creating a new club for rugby
+rclub_description <- c("club for rugby matches and practising rugby in general")
+rclub_dues <- c(20,10,5)
+rmeeting_days <- c("Tuesday", "Thursday")
+rmeeting_time <- c("7:00 am", "9:00 am")
+rclub_meeting_times <- rbind(rmeeting_days, rmeeting_time)
+rugby_club <- list(rclub_title, rclub_description, rclub_dues, rclub_meeting_times)
+rugby_club # created a list for rugby club
+rfirst_years <- c(10, 15)
+names(rfirst_years) <- c("fall", "spring")
+rugby_club[[5]] <- rfirst_years # appended first years student enrollment
+rclub_names <- c("club_title", "club_description", "club_dues", "club_meeting_times", "rfirst_years")
+names(rugby_club) <- rclub_names # assigning titles for each of the objects in the rugby club
+rugby_club
+cclub_title <- c("chess_Club") #creating a new club for chess
+cclub_description <- c("club for chess matches and practising chess in general")
+cclub_dues <- c(15,12,8)
+cmeeting_days <- c("monday", "wednesday")
+cmeeting_time <- c("6:00 pm", "8:00 pm")
+cclub_meeting_times <- rbind(cmeeting_days,cmeeting_time)
+chess_club <- list(cclub_title, cclub_description, cclub_dues, cclub_meeting_times)
+chess_club # created a list for rugby club
+cfirst_years <- c(20, 25)
+names(cfirst_years) <- c("fall", "spring")
+chess_club[[5]] <- cfirst_years # appended first years student enrollment
+cclub_names <- c("club_title", "club_description", "club_dues", "club_meeting_times", "cfirst_years")
+names(chess_club) <- cclub_names # assigning titles for each of the objects in the rugby club
+chess_club
+uni_list <- list(chess_club, rugby_club) # created a overall list to add both rugby and chess to it
+uni_list
