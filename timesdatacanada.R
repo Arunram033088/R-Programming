27c20b3026d86a8d1dbcf3914842428a8596ccd7
Arun Ramanathan <arunramanathan3@gmail.com>
2020-01-22 17:34:00 +0530

    To find out the top universities of Canada for the year 2016 based on world_rank and number of students more than 25000

---
+++ timesdatacanada.R
@@ -0,0 +1,13 @@
+library(readr)
+library(dplyr)
+timesdata <- read_csv("D:/Dataset/world-university-rankings/timesData.csv")
+timesfirstdata <- timesdata %>%
+  select("world_rank", "university_name", "country", "num_students", "year") #Selecting rank, university name, country, year
+timesdata2016 <- timesfirstdata %>%
+  filter(year == 2016)
+timesdata2016 <- timesdata2016 %>%
+  filter(country == "Canada")
+timesdata2016 <- timesdata2016 %>%
+  filter(num_students > 25000)
+timesdata2016 <- timesdata2016 %>%
+  filter(world_rank < 50 & world_rank != "201-250" & world_rank != "251-300" & world_rank != "301-350" & world_rank != "351-400" & world_rank != "401-500")
